d5fb637c73d12662abfe9693db262a52cf95682c
jpfera55 <joaop@transoft.com.br>
2018-09-19 17:47:11 -0300

    first commit

---
+++ src/pages/intro/intro.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+
+/**
+ * Generated class for the IntroPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-intro',
+  templateUrl: 'intro.html',
+})
+export class IntroPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad IntroPage');
+
+    setTimeout(() => {
+      this.goToTabsPage();
+    },3000);  
+  }
+
+  goToTabsPage() {
+    this.navCtrl.push(LoginPage);
+  }
+
+}
